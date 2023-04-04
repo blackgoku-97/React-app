7a48a008630b552b13862b02311d462c699e9165
Ricardo Pérez <rpe19971210@gmail.com>
2023-04-03 20:30:10 -0400

    Nueva App

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './index.css'
+import { Navigation } from './routes/Navigation'
+
+function App() {
+  return (
+    <>
+      <Navigation />
+    </>
+  )
+}
+
+export default App
