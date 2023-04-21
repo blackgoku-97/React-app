bad4afaeb0da711303be5602781b264dadeda7d3
Ricardo Pérez <rpe19971210@gmail.com>
2023-04-21 15:01:09 -0400

    dynamic forms listo

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -4,11 +4,13 @@ import { Routes, Route, NavLink, Navigate } from 'react-router-dom';
 import logo from '../assets/react.svg';
 
 import {
+    DynamicForm,
     FormikAbstraction,
     FormikBasicPage,
     FormikComponents,
     FormikYupPage,
-    RegisterPage
+    RegisterPage,
+    RegisterFormikPage
 } from '../03-forms/pages';
 
 export const Navigation = () => {
@@ -34,7 +36,10 @@ export const Navigation = () => {
                         <NavLink to="/formik-abstraction" className={ ({ isActive }) => isActive ? 'nav-active' : '' }>Formik Abstraction</NavLink>
                     </li>
                     <li>
-                        <NavLink to="/users" className={ ({ isActive }) => isActive ? 'nav-active' : '' }>Users</NavLink>
+                        <NavLink to="/formik-register" className={ ({ isActive }) => isActive ? 'nav-active' : '' }>Formik Register</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/dynamic-form" className={ ({ isActive }) => isActive ? 'nav-active' : '' }>Dynamic Form</NavLink>
                     </li>
                 </ul>
             </nav>
@@ -44,8 +49,9 @@ export const Navigation = () => {
                 <Route path='/formik-yup' element={ <FormikYupPage /> } />
                 <Route path='/formik-components' element={ <FormikComponents /> } />
                 <Route path='/formik-abstraction' element={ <FormikAbstraction /> } />
-                <Route path='users' element={ <h1>Users</h1> } />
+                <Route path='/dynamic-form' element={ <DynamicForm /> } />
                 <Route path='/register' element={ <RegisterPage /> } />
+                <Route path='/formik-register' element={ <RegisterFormikPage /> } />
 
                 <Route path='/*' element={ <Navigate to="/home" replace /> } />
             </Routes>
